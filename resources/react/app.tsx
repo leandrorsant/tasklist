f301812c15e211c92424f53f788c5eb0070c02d2
Leandro <ramosdossantos.leandro@gmail.com>
2024-07-06 13:43:48 -0300

    Front-end done

---
+++ resources/react/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+
+import Main from "./Main";
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
+    <Main />
+);
